4e146f86704eee799d83a661915e7c4c10bb24c2
Dylan Saldana <dsaldana@protocol.prv>
2019-07-12 16:23:27 -0500

    End Of Section 5

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -39,7 +39,7 @@ namespace DatingApp.API.Controllers
             };
 
             var createdUser = await _repo.Register(userToCreate, userForRegisterDto.password);
-
+            
             return StatusCode(201);
         }
 
